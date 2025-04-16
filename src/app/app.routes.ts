cd231aff044774705602a7274d726b0ad80f568e
Tejaspatil0307 <rp1767407@gmail.com>
2025-04-16 17:57:36 +0530

    final header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/Dashboard/dashboard/dashboard.component';
-
+import { SettingsComponent } from './features/Master/settings/settings.component';
 export const routes: Routes = [
-    {
-        path: 'dashboard',
-        component: DashboardComponent
-    },
+    
+        {
+            path: 'dashboard',
+            component: DashboardComponent,
+          },
+          {
+            path: 'settings',
+            component: SettingsComponent,
+          },
+   
 ];
