391d4c15c9b72a66d3152ec3f55ee73e45166b50
Tejaspatil0307 <rp1767407@gmail.com>
2025-04-16 10:42:53 +0530

    angularsidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { LefSideNavComponent } from "./shared/lef-side-nav/lef-side-nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LefSideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
